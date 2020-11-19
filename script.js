81521bb617720ab3983031aa3e7da37d4dbe7f2d
Pierre-Yves <pybomt@gmail.com>
2020-11-19 15:02:19 +0100

    Ajout d'un selecteur d'instrument de musique

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const keys = document.querySelectorAll(".key");
+const soundSelectors = document.querySelectorAll('input[name="SoundProfil"]');
 
+for (var i = 0; i < soundSelectors.length; i++) {
+    soundSelectors[i].addEventListener('change', function(){selectedSound()})
+}
+
+function selectedSound(){
+    if(soundSelectors[0].checked == true)
+    {
+        return 0;
+    }
+    else if(soundSelectors[1].checked == true)
+    {
+        return 1;
+    }
+    else if(soundSelectors[2].checked == true)
+    {
+        return 2;
+    }
+    else if(soundSelectors[3].checked == true)
+    {
+        return 3;
+    }
+}
 
 for (var i = 0; i < keys.length; i++) {
     keys[i].addEventListener('mousedown', function (e) { var key = e.target.id; createNote(key) })
@@ -7,20 +30,20 @@ for (var i = 0; i < keys.length; i++) {
 
 function createNote(key) {
     if (key.length == 1) {
-        Synth.play(0, key, 3, 2)
+        Synth.play(selectedSound(), key, 3, 2)
     }
     else if (key.slice(1, 2) == 'S') {
         if (key.length == 3) {
             key = key.slice(0, 1);
-            Synth.play(0, key+'#', 4, 2);
+            Synth.play(selectedSound(), key+'#', 4, 2);
         }
         else {
             key = key.slice(0, 1);
-            Synth.play(0, key+'#', 3, 2);
+            Synth.play(selectedSound(), key+'#', 3, 2);
         }
     }
     else {
         key = key.slice(0, 1);
-        Synth.play(0, key, 4, 2);
+        Synth.play(selectedSound(), key, 4, 2);
     }
 }
